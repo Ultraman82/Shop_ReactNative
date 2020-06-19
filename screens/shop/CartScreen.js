7695029cb602d5a7245e53d89d1bde8bf5e15b9a
Changhyun Jeong <exelcior99@gmail.com>
2020-06-19 11:35:56 -0700

    first commit

---
+++ screens/shop/CartScreen.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+const CartScreen = () => {
+  return <div></div>;
+};
+const styles = StyleSheet.create({});
+export default CartScreen;
